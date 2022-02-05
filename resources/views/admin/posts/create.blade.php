b512196af745d9493acbc387821517046c3d24b0
zetasoft930 <celsomateussousa@gmail.com>
2022-02-05 14:02:31 +0100

    crud e validacao com bootstrap

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,20 +1,52 @@
 
-<h1>Registar Post</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
 
-@if ($errors->any())
-    <h4>Errors</h4>
-    <ul style="color: red">
-        @foreach ($errors->all() as $item)
-            <li>{{ $item }}</li>
-        @endforeach
-    </ul>
-@endif
+    <h1>Registar Post</h1>
 
-<form action="{{ route('create') }}" method="POST">
-@csrf
-    <input type="text" name="title" id="title" placeholder="Titulo do Post" value="{{old('title')}}" >
-    <textarea name="content" id="content" cols="30" rows="10"  >
-        {{old('content')}}
-    </textarea>
-    <button type="submit">Reigistar</button>
-</form>
+    @if ($errors->any())
+        <h4>Errors</h4>
+        <ul style="color: red">
+            @foreach ($errors->all() as $item)
+                <li>{{ $item }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    <form action="{{ route('create') }}" method="POST">
+    @csrf
+        <div class="row">
+            <div class="col-6">
+                <input class="form-control" type="text" name="title"
+                        id="title" placeholder="Titulo do Post"
+                        value="{{old('title')}}" >
+            </div>
+        </div>
+         <div class="row">
+            <div class="col-6">
+                <textarea class="form-control"
+                name="content" id="content"
+                cols="30" rows="10"  >
+                    {{old('content')}}
+                </textarea>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <button class="btn btn-primary" type="submit">Reigistar</button>
+            </div>
+        </div>
+    </form>
+
+</body>
+</html>
+
+
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,7 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Curso Laravel - Posts</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
 
-<a href="{{ route('post.create')}}"> Registar Novo Post</a>
-<h1>Posts</h1>
 
-@foreach ( $posts as $post)
-    <p>{{  $post->title }}</p>
-@endforeach
+    <h1>Posts</h1>
+    <div class="row">
+        <div class="col-md-4">
+            <a class="btn btn-success" href="{{ route('post.create')}}"> Novo</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Content</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    @foreach ($posts as $post)
+                        <tr>
+                            <th scope="row">{{$loop->index+1}}</th>
+                            <td>{{ $post->title }}</td>
+                            <td>{{ $post->content }}</td>
+                        </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
